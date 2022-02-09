75547f6dd4b53542701b72aa193c6d4cbbd13adb
ianmat55 <ianmat55@gmail.com>
2022-02-09 09:24:06 -0800

    possibly fixed spacing issue with offset

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -11,7 +11,7 @@ const Cara = () => (
     <Parallax pages={4}>
       <Hero offset={0} factor={1} />
       <Projects offset={1} factor={2} />
-      <Contact offset={2} factor={3} />
+      <Contact offset={3} factor={1} />
       {/* <About offset={1} factor={1.2} /> */}
     </Parallax>
   </Layout>
