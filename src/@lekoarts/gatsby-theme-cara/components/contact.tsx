1ee9d9f7a5d67f134fe1b148e6dea581a6db0f8e
ianmat55 <ianmat55@gmail.com>
2022-02-09 08:34:16 -0800

    removed photo for smaller devices and increased page size

--- src/@lekoarts/gatsby-theme-cara/components/contact.tsx
+++ src/@lekoarts/gatsby-theme-cara/components/contact.tsx
@@ -12,6 +12,23 @@ const Contact = ({ offset, factor = 1 }: { offset: number; factor?: number }) =>
       <Inner>
         <ContactMDX />
       </Inner>
+      {/* <form name="contact" method="POST" data-netlify="true">
+        <div>
+          <label>Your Name: </label>
+          <input type="text" name="name" />   
+        </div>
+        <div>
+          <label>Your Email: </label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label>Message: </label>
+          <textarea name="message"></textarea>
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form> */}
     </Content>
    
   </div>
--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -10,8 +10,8 @@ const Cara = () => (
   <Layout>
     <Parallax pages={3}>
       <Hero offset={0} factor={1} />
-      <Projects offset={1} factor={.8} />
-      <Contact offset={2} factor={.8} />
+      <Projects offset={1} factor={1} />
+      <Contact offset={2} factor={1} />
       {/* <About offset={1} factor={1.2} /> */}
     </Parallax>
   </Layout>
